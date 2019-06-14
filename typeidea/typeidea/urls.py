ebaf892bb93900c1cd826e00f8cde17f4d649249
zpz <979994367@qq.com>
2019-06-14 16:27:20 +0800

    配置django-rest-framework及接口文档

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -19,6 +19,8 @@ from django.contrib.sitemaps import views as sitemap_views
 from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
+from rest_framework.documentation import include_docs_urls
 
 from .custom_site import custom_site
 from config.views import LinkListView
@@ -29,11 +31,14 @@ from blog.views import (
 )
 from blog.rss import LatestPostFeed
 from blog.sitemap import PostSitemap
+from blog.apis import PostViewSet
 from .autocomplete import CategoryAutocomplete, TagAutocomplete
 
 import xadmin
 
- 
+router = DefaultRouter()
+router.register(r'post', PostViewSet, base_name='api-post')
+
 urlpatterns = [
     url(r'^$', IndexView.as_view(),name='index'),
     url(r'^category/(?P<category_id>\d+)/$', CategoryView.as_view(), name='category-list'),
@@ -48,6 +53,7 @@ urlpatterns = [
     url(r'^admin/', xadmin.site.urls, name='xadmin'),
     url(r'^category-autocomplete/$', CategoryAutocomplete.as_view(), name='category-autocomplete'),
     url(r'^tag-autocompletes/$', TagAutocomplete.as_view(), name='tag-autocomplete'),
-    url(r'^ckeditor/',include('ckeditor_uploader.urls')),
-    #url(r'^super_admin/', admin.site.urls, name='super-admin'),
+    url(r'^ckeditor/', include('ckeditor_uploader.urls')),
+    url(r'^api/', include(router.urls, namespace="api")),
+    url(r'^api/docs/', include_docs_urls(title='typeidea apis')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #static用来配置图片资源访问，正式环境中实验nginx
